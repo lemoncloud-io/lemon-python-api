1df2dc9a609a34f7d387043536b9fa569825cad3
Steve <steve@lemoncloud.io>
2019-01-16 01:28:53 +0900

    method PUT with json

--- src/hello.py
+++ src/hello.py
@@ -6,13 +6,15 @@ from flask import (
     Blueprint, flash, g, redirect, render_template, request, session, url_for
 )
 
+from flask import jsonify, abort
+
 bp = Blueprint('hello', __name__, url_prefix='/hello')
 
 @bp.route("/", methods=['GET'])
 def hello():
     """
     example:
-    $ http localhost:8201/hello/
+    $ http :8201/hello/
     """
     import os
     buf = []
@@ -26,6 +28,29 @@ def hello():
 def getHello(id):
     """
     example:
-    $ http localhost:8201/hello/1
+    $ http :8201/hello/1
     """
     return "Hello ID:"+id
+
+@bp.route("/<int:id>", methods=['PUT'])
+def putHello(id):
+    """
+    - req/res by json
+
+    example:
+    $ echo '{"a":1}' | http PUT ':8201/hello/1?name=lemon'
+    """
+    mth = request.method                                                # must be PUT
+    cty = request.content_type                                          # must be 'application/json'
+    cec = request.content_encoding                                      # must be None
+    # jsn = request.get_json(force=False, silent=False, cache=True)       # parse input as json.
+    jsn = request.json
+    err = None
+
+    # name = request.form['name']           # post parameter
+    name = request.args['name']             # url parameter (see http://flask.pocoo.org/docs/1.0/api/#flask.Request)    
+    err = 'Unknown'
+
+    # return "Hello ID:%d - %s %s / %s %s <- %s"%(id, mth, name, cty, cec, jsonify(jsn))
+    # abort(404, "Post id {0} doesn't exist.".format(id))
+    return jsonify(name=name, method=mth, type=cty, input=jsn)          # response as json.
